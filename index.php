61efed47d42e0dd0b92bf5712ad849cc98ca39e1
ingdannyhernandez <ingdannyhernandez@gmail.com>
2014-10-15 17:29:47 -0430

    configuracion inicial
    se inicio con la aplicacion web procon quedadno instalada en el servidor
    condorserver

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>PROCON</title>
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+	<link href="estilo.css" rel="stylesheet" type="text/css">
+     <link rel="icon" type="image/x-icon" href="image/favicon.ico">
+</head>
+
+<body>
+	<header>
+		<h1>Bienvenidos a mi Sitio</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="#" title="Primer elemento">Uno</a></li>
+			<li><a href="#">Dos</a></li>
+			<li><a href="#">Trest</a></li>
+		</ul>
+	</nav>
+	<section>
+		<aside>
+<input type="date" name="fecha" >
+		</aside>
+		<content>
+			
+		</content>
+	</section>
+	<footer>
+		Todos los derechos reservados.
+	</footer>
+</body>
+</html> 

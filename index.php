f052dc004946c0aa662a0c176a27017a7c8dce70
Juan Motta <jemp23@gmail.com>
2014-10-22 11:38:39 -0430

    cambios en el index y la hoja de estilos

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 
 	<title>PROCON</title>
 	<link rel="stylesheet" type="text/css" href="normalize.css">
@@ -12,16 +14,27 @@
 	<header>
 		<h1><img src="image/logo.png" width="350" height="88"></h1>
 	</header>
-	<nav>
-		<ul>
-		  <li></li>
-			<li></li>
-			<li></li>
-		</ul>
-	</nav>
-	<section></section>
-	<footer>
-		Todos los derechos reservados.
-	</footer>
+<content>
+  <div class="formClave">
+    <h1>Conectar con PROCON</h1>
+    			<form name="form1" method="post" action="verificarIngreso.php">
+   			    
+                <p>
+   			    <label for="usu_log">Usuario</label>
+   			    <input type="text" name="usu_log" id="usu_log"><br>
+                </p>
+
+		        <p>
+		          <label for="usu_pass">Contraseña</label>
+		          <input type="password" name="usu_pass" id="usu_pass"><br>
+
+                 </p>
+                  <p>
+                  <input name="submit" type="submit" class="boton1" value="INGRESAR">
+                  </p>
+   			  </form>
+   			</div>
+</content>
+
 </body>
 </html> 
